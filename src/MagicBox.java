8a5ad7cf2a9a7676a02aa63da2d36c3770cacf14
Dmitriy <kravchenkodmitriy38@gmail.com>
2022-10-16 19:24:08 +0300

    Change code

--- src/MagicBox.java
+++ src/MagicBox.java
@@ -1,31 +1,32 @@
 import java.util.Random;
 
 public class MagicBox<T> {
-        protected T[] items;
+    protected T[] items;
 
-        public MagicBox(int sizeBox){
-            items = (T[]) new Object[sizeBox] ;
-        }
-        public boolean add(T item){
-            for (int i = 0; i < items.length; i++) {
-                if (items[i] == null){
-                    items[i] = item;
-                    return true;
-                }
-            }
-            System.out.println("Места в коробченке нет");
-            return false;
-        }
+    public MagicBox(int sizeBox) {
+        items = (T[]) new Object[sizeBox];
+    }
 
-        public T pick(){
-             for (int i = 0; i < items.length; i++) {
-                if(items[i] == null){
-                    throw new RuntimeException("В коробке еще есть место, можно добавить " + (items.length - i));
+    public boolean add(T item) {
+        for (int i = 0; i < items.length; i++) {
+            if (items[i] == null) {
+                items[i] = item;
+                return true;
             }
         }
-            Random random = new Random();
-            int randomInt = random.nextInt(items.length);
-            return items[randomInt];
+        System.out.println("Места в коробченке нет");
+        return false;
+    }
+
+    public T pick() {
+        for (int i = 0; i < items.length; i++) {
+            if (items[i] == null) {
+                throw new RuntimeException("В коробке еще есть место, можно добавить " + (items.length - i));
+            }
+        }
+        Random random = new Random();
+        int randomInt = random.nextInt(items.length);
+        return items[randomInt];
 
     }
 }
